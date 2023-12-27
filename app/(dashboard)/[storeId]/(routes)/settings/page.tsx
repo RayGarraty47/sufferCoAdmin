41ffdda5cfbf45b49420b70e4f5afa787fdb3194
Ray Garraty <raygarratyslastcookie@gmail.com>
2023-12-26 22:03:07 -0500

    confusing billboard API routing and modules, uses cloudinary for object storage

--- app/(dashboard)/[storeId]/(routes)/settings/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -34,9 +34,7 @@ const SettingsPage: React.FC<SettingsPageProps> = async ({
     return (
     <div className="flex-col">
         <div className="flex-1 space-y-4 p-8 pt-6">
-            <SettingsForm initialData={store}
-            
-            />
+            <SettingsForm initialData={store}/>
         </div>
     </div>
    );
